c03d0954e09bf8f9c0b81a5a9ed7c5e9f025ac97
Filip Belik <fbelik@gustavus.edu>
2020-03-02 12:02:07 -0600

    Created blank activities and layout files for all screens

---
+++ app/src/main/java/com/example/gustavustutoringapp/QuestionForum.kt
@@ -0,0 +1,12 @@
+package com.example.gustavustutoringapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionForum : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.question_forum)
+    }
+}
---
+++ app/src/main/java/com/example/gustavustutoringapp/TutorView.kt
@@ -0,0 +1,12 @@
+package com.example.gustavustutoringapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tutor_view)
+    }
+}
